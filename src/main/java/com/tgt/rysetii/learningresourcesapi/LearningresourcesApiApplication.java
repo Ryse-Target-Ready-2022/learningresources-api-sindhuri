d89eec3e29895a3782433fd2542dee7f7cc53644
Sindhuri03 <Sindhuri20014@gmail.com>
2022-07-05 10:22:03 +0530

    <Changes are done>

--- src/main/java/com/tgt/rysetii/learningresourcesapi/LearningresourcesApiApplication.java
+++ src/main/java/com/tgt/rysetii/learningresourcesapi/LearningresourcesApiApplication.java
@@ -8,6 +8,7 @@ public class LearningresourcesApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningresourcesApiApplication.class, args);
+		System.out.println("Hello World");
 
 	}
 
