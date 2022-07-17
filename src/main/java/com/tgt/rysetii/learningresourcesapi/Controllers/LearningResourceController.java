95844628581d786f6004d949a77d0cf8451415e9
Sindhuri03 <Sindhuri20014@gmail.com>
2022-07-17 15:09:45 +0530

    Created controllers package and LearningResourceController class and created Get,Post and Delete endpoints to interact with

---
+++ src/main/java/com/tgt/rysetii/learningresourcesapi/Controllers/LearningResourceController.java
@@ -0,0 +1,41 @@
+package com.tgt.rysetii.learningresourcesapi.Controllers;
+import com.tgt.rysetii.learningresourcesapi.Repositories.LearningResourceRepository;
+import com.tgt.rysetii.learningresourcesapi.entity.LearningResource;
+import com.tgt.rysetii.learningresourcesapi.service.LearningResourceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class LearningResourceController {
+
+    private final LearningResourceService lrs;
+    @Autowired
+    LearningResourceRepository a;
+
+    public LearningResourceController(LearningResourceService lrs) {
+        this.lrs = lrs;
+    }
+
+    @GetMapping("/Resources")
+        public List<LearningResource> getResources()
+    {
+        return lrs.getLearningResources();
+    }
+    @PostMapping("/Resources")
+    public String putResources(@RequestBody List<LearningResource> lr)
+    {
+
+            lrs.saveLearningResources(lr);
+            return "Added the Resources Successfully";
+    }
+    @DeleteMapping("/Resources/{id}")
+    public String deleteResource(@PathVariable int id)
+    {
+        return lrs.deleteLearningResource(id);
+    }
+
+
+}
